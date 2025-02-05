0aa2613177820ecc6f910c9b4a8da399e88ae35f
ckdrms <ckdrmsdl9999@naver.com>
2025-02-06 01:00:39 +0900

    관심 상품에 폴더 추가 및 폴더 전체 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -3,10 +3,10 @@ package com.sparta.myselectshop.service;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
-import com.sparta.myselectshop.entity.Product;
-import com.sparta.myselectshop.entity.User;
-import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.entity.*;
 import com.sparta.myselectshop.naver.dto.ItemDto;
+import com.sparta.myselectshop.repository.FolderRepository;
+import com.sparta.myselectshop.repository.ProductFolderRepository;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +26,8 @@ public class ProductService {
     public static final int MIN_MY_PRICE = 100;
 
     private final ProductRepository productRepository;
-
+    private final FolderRepository folderRepository;
+    private final ProductFolderRepository productFolderRepository;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
         Product product = productRepository.save(new Product(requestDto, user));//ctrl+b 생성자이동
@@ -35,13 +37,13 @@ public class ProductService {
     @Transactional//dirty checking(변경감지)
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
-        if(myprice<MIN_MY_PRICE){//BEAN VALIDATION으로 제어해도좋음
-            throw new IllegalArgumentException("유효하지 않은 관심가격이빈다 최소 "+MIN_MY_PRICE+
+        if (myprice < MIN_MY_PRICE) {//BEAN VALIDATION으로 제어해도좋음
+            throw new IllegalArgumentException("유효하지 않은 관심가격이빈다 최소 " + MIN_MY_PRICE +
                     "원 이상으로 설정해주세요");
         }
 
-        Product product=productRepository.findById(id).orElseThrow(()
-                ->new NullPointerException("해당상품을 찾을 수 없습니다"));
+        Product product = productRepository.findById(id).orElseThrow(()
+                -> new NullPointerException("해당상품을 찾을 수 없습니다"));
 
         product.update(requestDto);
         return new ProductResponseDto(product);
@@ -50,16 +52,16 @@ public class ProductService {
     @Transactional(readOnly = true)
     public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {//함수형으로 한줄로해보기
 
-        Sort.Direction direction=isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
         Pageable pageable = PageRequest.of(page, size, sort);
 
         UserRoleEnum userRoleEnum = user.getRole();
         Page<Product> productList;
-        if(userRoleEnum==userRoleEnum.USER){
-            productList=productRepository.findAllByUser(user,pageable);
-        }else {
-            productList=productRepository.findAll(pageable);
+        if (userRoleEnum == userRoleEnum.USER) {
+            productList = productRepository.findAllByUser(user, pageable);
+        } else {
+            productList = productRepository.findAll(pageable);
         }
 
 //        List<ProductResponseDto> responseDtoList=new ArrayList<>();
@@ -72,22 +74,35 @@ public class ProductService {
         return productList.map(ProductResponseDto::new);
 
     }
+
     @Transactional
     public void updateBySearch(Long id, ItemDto itemDto) {
-    Product product = productRepository.findById(id).orElseThrow(()->new NullPointerException("해당" +
-    "상품은 존재하지 않습니다")
-    );
-     product.updateByItemDto(itemDto);
+        Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("해당" +
+                "상품은 존재하지 않습니다")
+        );
+        product.updateByItemDto(itemDto);
     }
 
-//    public List<ProductResponseDto> getAllProducts() { //페이징추가하면 또만들어야되서
-//        List<ProductResponseDto> responseDtoList=new ArrayList<>();
-//        List<Product> products=productRepository.findAll(); //이것도 altenter되네
-//
-//        for (Product product : products) {
-//            responseDtoList.add(new ProductResponseDto(product));
-//
-//        }
-//        return responseDtoList;
-//    }
+    public void addFolder(Long productId, Long folderId, User user) {
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new NullPointerException("해당상품이 존재하지 않습니다")
+        );
+
+        Folder folder = folderRepository.findById(folderId).orElseThrow(
+                () -> new NullPointerException("해당폴더가 존재하지 않습니다")
+        );
+
+        if (!product.getUser().getId().equals(user.getId()) || !folder.getUser().getId().equals(user.getId())) {
+            throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 회원님의 폴더가 아닙니다");
+        }
+
+        Optional<ProductFolder> overlapFolder = productFolderRepository.findByProductAndFolder(product, folder);
+
+        if (overlapFolder.isPresent()) {
+            throw new IllegalArgumentException("중복된 폴더입니다.");
+        }
+
+        productFolderRepository.save(new ProductFolder(product, folder));
+    }
+
 }
